bac8ecdf83aa1595cc4370bcb7c03df63f05ede2
Kensu <khentandreitenchavez@gmail.com>
2026-08-02 12:53:44 +0800

    Add LazyMotion for lighter animation bundle

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,16 @@
 'use client';
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from '@/lib/theme';
+import { LazyMotion, domAnimation } from 'framer-motion';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-      <ThemeProvider>{children}</ThemeProvider>
+      <ThemeProvider>
+        <LazyMotion features={domAnimation} strict>
+          {children}
+        </LazyMotion>
+      </ThemeProvider>
     </SessionProvider>
   );
 }
